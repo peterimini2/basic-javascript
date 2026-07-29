099d5a1b7f4f653f61af6262f3c69389fa9be38d
Peter Imini <peterimini2@gmail.com>
2026-07-29 12:27:06 +0100

    add cost of order in dom page

--- js/dom.js
+++ js/dom.js
@@ -32,3 +32,29 @@ function subscribe() {
     : subscribeBtn.innerHTML = 'Subscribe';
 }
 
+//Cost of Order
+const orderInput = document.querySelector('.order-input');
+const calcBtn = document.querySelector('.calc-btn');
+const totalCost = document.querySelector('.cost');
+
+calcBtn.addEventListener("click", () => {
+  calculateTotal();
+})
+
+orderInput.addEventListener("keydown", () => {
+  if(event.key === "Enter") {
+  calculateTotal();
+  }
+})
+
+function calculateTotal() {
+  let cost = Number(orderInput.value);
+
+  if (orderInput.value.trim() === "") {
+    totalCost.innerHTML = 'Please input cost of order.'
+  } else if (cost < 40) {
+    totalCost.innerHTML = `$${ cost + 10 }`
+  } else {
+    totalCost.innerHTML = `$${cost}`
+  }
+}
