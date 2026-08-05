d7925f098f4b0b8083bce0aae593cf2b61dc5adc
Peter Imini <peterimini2@gmail.com>
2026-08-05 10:27:26 +0100

    add keydown event on body

--- js/advance-function.js
+++ js/advance-function.js
@@ -63,7 +63,7 @@ let intervalId;
 
 function autoPlay() {
   if (!isAutoPlaying) {
-    intervalId = setInterval(function() {
+    intervalId = setInterval(() => {
     const playerMove = pickComputerMove();
     playGame(playerMove);
     }, 1000);
@@ -74,6 +74,15 @@ function autoPlay() {
 }
 }
 
+document.body.addEventListener('keydown', (event) => {
+  if (event.key === 'r') {
+    playGame('rock');
+  } else if (event.key === 'p') {
+    playGame('paper');
+  } else if (event.key === 's') {
+    playGame('scissors');
+  }
+});
 
 function playGame(playerMove) {
   const computerMove = pickComputerMove();
